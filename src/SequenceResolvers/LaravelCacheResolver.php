ddf7ab9e2547373eb52969ea9ab504650785268f
Bradie Tilley <bradie@hatchet.com.au>
2025-02-02 10:26:27 +0800

    fix: ensure cache is locked when incrementing

--- src/SequenceResolvers/LaravelCacheResolver.php
+++ src/SequenceResolvers/LaravelCacheResolver.php
@@ -4,13 +4,17 @@ namespace BradieTilley\Snowflakes\SequenceResolvers;
 
 use BradieTilley\Snowflake\SequenceResolvers\SequenceResolver;
 use Illuminate\Cache\CacheManager;
+use Illuminate\Contracts\Cache\Lock;
 use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Support\Facades\Cache;
 
 class LaravelCacheResolver implements SequenceResolver
 {
     protected string $prefix;
 
     protected Repository $cache;
+
+    protected Lock $lock;
 
     public function __construct(CacheManager $cache)
     {
@@ -21,16 +25,21 @@ class LaravelCacheResolver implements SequenceResolver
         /** @var string $prefix */
         $prefix = config('snowflakes.sequencing.prefix', '');
         $this->prefix = $prefix;
+
+        $this->lock = Cache::lock("{$prefix}_lock", 10);
     }
 
     public function sequence(int $currentTime): int
     {
         $key = $this->prefix.$currentTime;
 
-        if ($this->cache->add($key, 1, 10)) {
-            return 0;
-        }
+        return $this->lock->block(10, function () use ($key) {
+            if ($this->cache->add($key, 1, 10)) {
+                return 0;
+            }
 
-        return $this->cache->increment($key) | 0;
+            return $this->cache->increment($key) | 0;
+        });
+
     }
 }
